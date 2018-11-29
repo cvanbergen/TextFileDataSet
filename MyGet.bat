cb5bfefb56cd66d74b0307cf815d795eadab8bbc
cvanbergen <cvanbergen@github.com>
2018-11-29 14:57:15 +0100

    Adjusting MyGet.bat

--- MyGet.bat
+++ MyGet.bat
@@ -15,5 +15,9 @@ if "%nuget%" == "" (
 )
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild TextFileDataSet\TextFileDataSet.csproj /p:Configuration="%config%"  /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild TextFileDataSet_v30\TextFileDataSet_v30.csproj /p:Configuration="%config%"  /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild TextFileDataSet_v35\TextFileDataSet_v35.csproj /p:Configuration="%config%"  /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild TextFileDataSet_v40\TextFileDataSet_v40.csproj /p:Configuration="%config%"  /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild TextFileDataSet_v47\TextFileDataSet_v47.csproj /p:Configuration="%config%"  /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 %nuget% pack "TextFileDataSet.nuspec" -NoPackageAnalysis -verbosity detailed -Version %version% -p Configuration="%config%"
